c500aa9b3af2fbff712eea247a65b5315d54ca2a
Leo Salat <leo.salat@gmail.com>
2017-03-12 21:40:56 -0700

    add additional info to tracks array

--- frontend/util.js
+++ frontend/util.js
@@ -1,19 +1,22 @@
-const parseAlbumsResponse = (albums) => (
-  albums.map(a => (
-      {
-    		id: a.id,
-    		name: a.name,
-    		artists: a.artists.map(a => a.name).join(', '),
-    		tracks: a.tracks.items.map(t => (
-    				{
-    					id: t.id,
-    					track: t.name,
-    					track_number: t.track_number,
-    					duration_ms: t.duration_ms
-    				}
-    			)
-        );
-      }
-    )
-  );
-);
+var parseAlbumResponse = (albums) => {
+  return albums.albums.map(album => {
+
+    const tracks = album.tracks.items.map(t => {
+      return {
+        id: t.id,
+        artist: t.artists.map(artist => artist.name).join(', '),
+        album: album.name,
+        track: t.name,
+        track_number: t.track_number,
+        duration_ms: t.duration_ms
+      };
+    });
+
+    return {
+      id: album.id,
+      name: album.name,
+      artists: album.artists.map(artist => artist.name).join(', '),
+      tracks
+    };
+  });
+};
