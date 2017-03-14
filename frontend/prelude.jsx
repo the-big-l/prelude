ba9e0403842647c0be5e720e2b4215f297a5150d
Leo Salat <leo.salat@gmail.com>
2017-03-14 15:14:03 -0700

    add auth to window for testing

--- frontend/prelude.jsx
+++ frontend/prelude.jsx
@@ -1,7 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import configureStore from './store/store';
+
+// test
+import { login, signup, logout } from './actions/session_actions';
 
 document.addEventListener('DOMContentLoaded', () => {
     const root = document.getElementById('root');
+
+    const store = window.currentUser ?
+      configureStore({session: {currentUser: window.currentUser}}) :
+      configureStore();
+
+    // Testing
+    window.login = login;
+    window.signup = signup;
+    window.logout = logout;
+    window.store = store;
+
     ReactDOM.render(<h1>Welcome to Prelude</h1>, root);
 });
