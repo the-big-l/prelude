f3267c443ab9eefdf61fdef360e5325d7a011986
Leo Salat <leo.salat@gmail.com>
2017-03-14 11:50:08 -0700

    add create/index methods

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,2 +1,22 @@
 class Api::UsersController < ApplicationController
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      login!(@user)
+      render :show
+    else
+      render json: @users.errors.full_messages, status: 422
+    end
+  end
+
+  def index
+    @users = Users.all
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :password)
+  end
 end
