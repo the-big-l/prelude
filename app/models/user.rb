8297b11c5ed06c098f8be5e2bb82f70a7a643898
Leo Salat <leo.salat@gmail.com>
2017-03-14 14:26:19 -0700

    add password length validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
 
   validates :username, :session_token, presence: true, uniqueness: true
   validates :password_digest, presence: true
+  validates :password, length: { minimum: 8, allow_nil: true }
 
   after_initialize :ensure_session_token
   before_validation :ensure_session_token_uniqueness
