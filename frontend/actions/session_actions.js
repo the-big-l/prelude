03a39427bf4df95009d9fa18420c3a12d0298ada
Leo Salat <leo.salat@gmail.com>
2017-03-14 15:02:26 -0700

    add LOGOUT constant

--- frontend/actions/session_actions.js
+++ frontend/actions/session_actions.js
@@ -2,6 +2,7 @@ import * as APIUtil from '../util/session_api_util'
 
 export const RECEIVE_CURRENT_USER = "RECEIVE_CURRENT_USER";
 export const RECEIVE_ERRORS = "RECEIVE_ERRORS";
+export const LOGOUT = "LOGOUT";
 
 // sync
 export const receiveCurrentUser = currentUser => ({
