71286f67a1d9eececbaa79b4f2332c99d220a2c7
Leo Salat <leo.salat@gmail.com>
2017-03-14 11:37:46 -0700

    add user model

---
+++ app/models/user.rb
@@ -0,0 +1,46 @@
+class User < ApplicationRecord
+  attr_reader :password
+
+  validates :username, :session_token, presence: true, uniqueness: true
+  validates :password_digest, presence: true
+
+  after_initialize :ensure_token
+  before_validation :ensure_token_uniqueness
+
+  def self.find_by_credentials(username, password)
+    user = User.find_by_username(username)
+    user && user.valid_password?(password) ? user : nil
+  end
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def reset_session_token!
+    self.session_token = new_session_token
+    ensure_session_token_uniqueness
+    self.save
+    self.session_token
+  end
+
+  def valid_password?(password)
+    BCrypt::Password.new(password_digest).is_password?(password)
+  end
+
+  private
+
+  def generate_token
+    SecureRandom.urlsafe_base64(16)
+  end
+
+  def ensure_token
+    self.session_token ||= generate_token
+  end
+
+  def ensure_session_token_uniqueness
+    while User.find_by(session_token: self.session_token)
+      self.session_token = generate_token
+    end
+  end
+end
