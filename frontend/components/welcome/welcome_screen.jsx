06c86ee63602286d37cdcf9cc50939b8d68f80ac
Leo Salat <leo.salat@gmail.com>
2017-03-15 10:19:13 -0700

    add render logic based on route

---
+++ frontend/components/welcome/welcome_screen.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import SignupFlowContainer from '../session/signup_flow_container';
+import LoginFormContainer from '../session/login_form_container';
+import SignupFormContainer from '../session/signup_form_container';
+import LoginFeatures from './login_features';
+
+class WelcomeScreen extends React.Component {
+  renderLoginMethod()  {
+    switch (this.props.currentScreen) {
+      case 'login':
+        return <LoginFormContainer />;
+      case 'signup':
+        return <SignupFormContainer />;
+      default:
+        return <SignupFlowContainer />;
+    }
+  }
+
+  render() {
+    return (
+      <div className='welcome-screen'>
+        {this.renderLoginMethod()}
+        <LoginFeatures />
+      </div>
+    );
+  }
+}
+
+export default WelcomeScreen;
