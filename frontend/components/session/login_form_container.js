841c939aa8ec00198857d08ecfdd4b7a173036af
Leo Salat <leo.salat@gmail.com>
2017-03-14 17:35:14 -0700

    explicit component names

--- frontend/components/session/login_form_container.js
+++ frontend/components/session/login_form_container.js
@@ -1,5 +1,5 @@
 import { connect } from 'react-redux';
-import LoginForm from './session_form';
+import LoginForm from './login_form';
 import { login } from '../../actions/session_actions';
 
 const mapStateToProps = ({ session }) => ({
