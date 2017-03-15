f3c2561f0b996ff2abfb340988b2fb4d61c9cf2d
Leo Salat <leo.salat@gmail.com>
2017-03-15 16:54:52 -0700

    add styling elements

--- frontend/components/session/signup_flow.jsx
+++ frontend/components/session/signup_flow.jsx
@@ -9,14 +9,19 @@ const handleGuestLogin = login => e => {
 
 const SignupFlow = ({ login }) => (
   <div className='signup-flow'>
-    <img src='https://play.spotify.edgekey.net/client/a59725d/images/logo.png' />
+    <div className='large-logo'>
+      <img src='https://play.spotify.edgekey.net/client/a59725d/images/logo.png' />
+    </div>
     <button className='guest' onClick={handleGuestLogin(login)}>Sign in as a guest</button>
     <Link to='/signup'>
       <button>Sign up with your email address</button>
     </Link>
-    <Link to='/login'>
-      <p className='text-small'>Already have an account? Log in here.</p>
-    </Link>
+    <hr/>
+    <div className='text-small has-account'>
+      <Link to='/login'>
+        Already have an account? Log in here.
+      </Link>
+    </div>
   </div>
 );
 
