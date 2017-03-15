eb72dd1f073ab3945c68067ee6743e28f4c47fb0
Leo Salat <leo.salat@gmail.com>
2017-03-15 10:18:21 -0700

    add login_features.jsx

---
+++ frontend/components/welcome/login_features.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const LoginFeatures = () => (
+<div className="login-features">
+  <h1>Get the right music, right now</h1>
+  <h2>Listen to millions of songs for free.</h2>
+  <ul>
+    <li>Search & discover music you'll love</li>
+    <li>Create playlists of your favorite music</li>
+    <li>Spotify Radio - unlimited stations and skips</li>
+  </ul>
+</div>
+);
+
+export default LoginFeatures;
