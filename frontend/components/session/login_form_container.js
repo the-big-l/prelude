9623b23875e98047ce1964dbb11f7cfc4425cd21
Leo Salat <leo.salat@gmail.com>
2017-03-14 17:15:50 -0700

    add props

--- frontend/components/session/login_form_container.js
+++ frontend/components/session/login_form_container.js
@@ -1,19 +1,15 @@
 import { connect } from 'react-redux';
-import SessionForm from './session_form';
-import { login, logout, signup } from '../../actions/session_actions';
+import LoginForm from './session_form';
+import { login } from '../../actions/session_actions';
 
 const mapStateToProps = ({ session }) => ({
   loggedIn: !!session.currentUser,
   errors: session.errors
 });
 
-const mapDispatchToProps = (dispatch, { location }) => {
-  const formType = location.pathname.slice(1);
-  const action = formType === 'login' ? login : signup;
-
+const mapDispatchToProps = dispatch => {
   return {
-    processForm: user => dispatch(action(user)),
-    formType
+    login: user => dispatch(login(user))
    };
 };
 
