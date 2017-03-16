6c92b8af825a3aa3cb711790e1558750d9773bd8
Leo Salat <leo.salat@gmail.com>
2017-03-16 10:13:32 -0700

    add main_content.jsx

---
+++ frontend/components/app/main/main_content.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class MainContent extends React.Compoenent {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    <div className='main'>
+      <h1>content goes here</h1>
+    </div>
+  }
+}
