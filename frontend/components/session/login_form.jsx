f5cf55341c9455ab9f3aa0d897e044238ccce134
Leo Salat <leo.salat@gmail.com>
2017-03-15 17:59:17 -0700

    add styling to session forms

--- frontend/components/session/login_form.jsx
+++ frontend/components/session/login_form.jsx
@@ -31,13 +31,17 @@ class LoginForm extends React.Component {
     this.props.login({user});
   }
 
-  navLink() {
-  return <Link to="/signup">sign up instead</Link>;
-  }
+  handleGuestLogin(login) {
+    return e => {
+      const user = {username: 'guest', password: 'password'};
+      login({user});
+      hashHistory.push('/');
+    }
+  };
 
   renderErrors() {
     return(
-      <ul>
+      <ul className='form-errors'>
         {this.props.errors.map((error, i) => (
           <li key={`error-${i}`}>
             {error}
@@ -50,22 +54,33 @@ class LoginForm extends React.Component {
   render() {
     return (
       <div className="login-form-container">
+        <div className='small-logo'>
+          <img src='https://play.spotify.edgekey.net/client/a59725d/images/logo.png' />
+        </div>
+        <button
+          className='guest'
+          onClick={this.handleGuestLogin(login)}>
+            Sign in as a guest
+        </button>
+        <h1 className='spacer'>or</h1>
         <form onSubmit={this.handleSubmit} className="login-form-box">
           {this.renderErrors()}
           <div className="login-form">
+            <label>Username</label>
             <input type="text"
-              placeholder="username"
+              placeholder="Prelude username"
               value={this.state.username}
               onChange={this.update("username")}
               className="login-input" />
 
+            <label>Password</label>
             <input type="password"
               placeholder="password"
               value={this.state.password}
               onChange={this.update("password")}
               className="login-input" />
 
-            <input type="submit" value="Submit" />
+            <button type="submit">Login</button>
           </div>
         </form>
       </div>
