c689be79f74e2da236c8e4b1653fdf923ee5b28c
Leo Salat <leo.salat@gmail.com>
2017-03-16 10:13:52 -0700

    add main_content_container.js

---
+++ frontend/components/app/main/main_content_container.js
@@ -0,0 +1,14 @@
+import MainContent from './login_form';
+
+const mapStateToProps = props => ({
+
+});
+
+const mapDispatchToProps = dispatch => ({
+
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(MainContent);
