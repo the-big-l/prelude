161e11315f0917e7c2f6a55db7c3f2bcbfb6151e
Leo Salat <leo.salat@gmail.com>
2017-03-15 23:12:26 -0700

    add lifecycle methods for redirects; fix redirect on guest login

--- frontend/components/session/signup_flow.jsx
+++ frontend/components/session/signup_flow.jsx
@@ -1,26 +1,45 @@
 import React from 'react';
-import { Link, hashHistory } from 'react-router';
+import { Link, withRouter } from 'react-router';
 
-const handleGuestLogin = login => e => {
-  const user = {username: 'guest', password: 'password'};
-  login({user});
-  hashHistory.push('/');
-};
 
-const SignupFlow = ({ login }) => (
-  <div className='signup-flow'>
-    <div className='large-logo'>
-      <img src='https://play.spotify.edgekey.net/client/a59725d/images/logo.png' />
-    </div>
-    <button className='guest' onClick={handleGuestLogin(login)}>Sign in as a guest</button>
-    <button onClick={e => hashHistory.push('/signup')}>Sign up with your email address</button>
-    <hr/>
-    <div className='text-small footer'>
-      <Link to='/login'>
-        Already have an account? Log in here.
-      </Link>
-    </div>
-  </div>
-);
+class SignupFlow extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleGuestLogin = this.handleGuestLogin.bind(this);
+  }
 
-export default SignupFlow;
+  componentDidUpdate() {
+    this.redirectIfLoggedIn();
+  }
+
+  redirectIfLoggedIn() {
+    if (this.props.loggedIn) {
+      this.props.router.push("/");
+    }
+  }
+
+  handleGuestLogin(e) {
+    const user = {username: 'guest', password: 'password'};
+    this.props.login({user});
+  }
+
+  render() {
+    return (
+      <div className='signup-flow'>
+        <div className='large-logo'>
+          <img src='https://play.spotify.edgekey.net/client/a59725d/images/logo.png' />
+        </div>
+        <button className='guest' onClick={this.handleGuestLogin}>Sign in as a guest</button>
+        <button onClick={e => this.props.router.push('/signup')}>Sign up with your email address</button>
+        <hr/>
+        <div className='text-small footer'>
+          <Link to='/login'>
+            Already have an account? Log in here.
+          </Link>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default withRouter(SignupFlow);
