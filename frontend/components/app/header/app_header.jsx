0184c0576df8630ebba7f54e96654c5ddff1d76d
Leo Salat <leo.salat@gmail.com>
2017-03-16 11:13:23 -0700

    add lifecycle methods

--- frontend/components/app/header/app_header.jsx
+++ frontend/components/app/header/app_header.jsx
@@ -1,18 +1,38 @@
 import React from 'react';
+import { Link, withRouter } from 'react-router';
 
-const AppHeader = ({currentUser, logout}) => (
-  <header className='app-header'>
-    <div className='right-nav'>
-      <div className='current-user-info'>
-        {currentUser.username}
-      </div>
-      <ul>
-        <li onClick={e => logout()}>
-          logout
-        </li>
-      </ul>
-    </div>
-  </header>
-);
+class AppHeader extends React.Component {
+  constructor(props) {
+    super(props);
+  }
 
-export default AppHeader;
+  componentDidUpdate() {
+    this.redirectUnlessLoggedIn();
+  }
+
+  redirectUnlessLoggedIn() {
+    if (!this.props.loggedIn) {
+      this.props.router.push("/welcome");
+    }
+  }
+
+  render() {
+    return (
+      <header className='app-header'>
+        <div className='right-nav'>
+          <div className='current-user-info'>
+            {this.props.currentUser.username}
+          </div>
+          <ul className='user-dropdown'>
+            <li onClick={this.props.logout}>
+              logout
+            </li>
+          </ul>
+        </div>
+      </header>
+    );
+  }
+}
+
+
+export default withRouter(AppHeader);
