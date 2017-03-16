6b416bd4a28a8cf737b23fcf2aa4355f5f9d2d27
Leo Salat <leo.salat@gmail.com>
2017-03-16 10:12:07 -0700

    add main_nav.jsx

---
+++ frontend/components/app/main_nav.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const MainNav = () => (
+  <div className='main-nav'>
+    <ul>
+      <li>Users</li>
+      <li>Browse</li>
+      <li>Playlists</li>
+    </ul>
+  </div>
+);
+
+export default MainNav;
