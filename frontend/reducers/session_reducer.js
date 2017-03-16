e662b2d66908d5106a1202a263e54012e26784e7
Leo Salat <leo.salat@gmail.com>
2017-03-16 12:10:28 -0700

    fix clearErrors to not clear user

--- frontend/reducers/session_reducer.js
+++ frontend/reducers/session_reducer.js
@@ -21,7 +21,7 @@ const sessionReducer = (state = _nullUser, action) => {
       const errors = action.errors;
       return merge({}, _nullUser, {errors});
     case CLEAR_ERRORS:
-      return merge({}, _nullUser);
+      return merge({}, state, {errors: []});
     default:
       return state;
   }
