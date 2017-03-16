b36a98211584b6b340a5e815c717b21678a06fee
Leo Salat <leo.salat@gmail.com>
2017-03-16 15:22:45 -0700

    add player_container.js

---
+++ frontend/components/app/player/player_container.js
@@ -0,0 +1,13 @@
+import { connect } from 'react-redux';
+import Player from './player';
+
+const mapStateToProps = ({ session }) => ({
+});
+
+const mapDispatchToProps = dispatch => ({
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Player);
