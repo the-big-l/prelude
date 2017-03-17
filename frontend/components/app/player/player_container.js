68b828fac540046c518438b13197f94ab1b4a4d1
Leo Salat <leo.salat@gmail.com>
2017-03-16 20:10:49 -0700

    add player controls

--- frontend/components/app/player/player_container.js
+++ frontend/components/app/player/player_container.js
@@ -1,10 +1,29 @@
 import { connect } from 'react-redux';
 import Player from './player';
+import {
+  togglePlay,
+  toggleLoop,
+  toggleMute,
+  previousSong,
+  nextSong,
+  changeVolume,
+  replaceQueue } from '../../../actions/player_actions';
 
-const mapStateToProps = ({ session }) => ({
+const mapStateToProps = ({ player }) => ({
+  playing: player.playing,
+  loop: player.loop,
+  mute: player.mute,
+  volume: player.volume,
+  source: player.queue[player.currentSong],
 });
 
 const mapDispatchToProps = dispatch => ({
+  togglePlay: () => dispatch(togglePlay()),
+  toggleLoop: () => dispatch(toggleLoop()),
+  toggleMute: () => dispatch(toggleMute()),
+  previousSong: () => dispatch(previousSong()),
+  nextSong: () => dispatch(nextSong()),
+  changeVolume: volume => dispatch(changeVolume(volume))
 });
 
 export default connect(
