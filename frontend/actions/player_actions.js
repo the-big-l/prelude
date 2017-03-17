9a5d297e31816ae90231716ec8b49706f5f265ba
Leo Salat <leo.salat@gmail.com>
2017-03-17 09:41:50 -0700

    add shuffle songs action

--- frontend/actions/player_actions.js
+++ frontend/actions/player_actions.js
@@ -3,6 +3,7 @@ export const TOGGLE_LOOP = "TOGGLE_LOOP";
 export const TOGGLE_MUTE = "TOGGLE_MUTE";
 export const PREVIOUS_SONG = "PREVIOUS_SONG";
 export const NEXT_SONG = "NEXT_SONG";
+export const SHUFFLE_SONGS = "SHUFFLE_SONGS";
 export const CHANGE_VOLUME = "CHANGE_VOLUME";
 export const REPLACE_QUEUE = "REPLACE_QUEUE";
 
@@ -25,6 +26,10 @@ export const previousSong = () => ({
 
 export const nextSong = () => ({
   type: NEXT_SONG
+});
+
+export const shuffleSongs = () => ({
+  type: SHUFFLE_SONGS
 });
 
 export const changeVolume = (volume) => ({
