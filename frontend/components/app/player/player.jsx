c5d772c9c40fcc8513eb3bfa59060a0dc2d5e56a
Leo Salat <leo.salat@gmail.com>
2017-03-17 09:39:53 -0700

    add play button toggle; refactor button tags

--- frontend/components/app/player/player.jsx
+++ frontend/components/app/player/player.jsx
@@ -6,6 +6,10 @@ class Player extends React.Component {
     super(props);
   }
 
+  togglePlayButton() {
+    return this.props.playing ? 'pause' : 'play'
+  }
+
   render() {
     const {
       togglePlay,
@@ -25,11 +29,26 @@ class Player extends React.Component {
         <div id='now-playing'>{source.slice(source.length - 20, -4)}</div>
         <div id='player-controls'>
           <div id='player-buttons'>
-            <button id='loop' onClick={e => toggleLoop()}></button>
-            <button id='prev-song' onClick={e => previousSong()}></button>
-            <button id='play-pause' onClick={e => togglePlay()}></button>
-            <button id='next-song' onClick={e => nextSong()}></button>
-            <button id='mute' onClick={e => toggleMute()}></button>
+            <button
+              className='loop'
+              onClick={e => toggleLoop()}>
+            </button>
+            <button
+              className='prev-song'
+              onClick={e => previousSong()}>
+            </button>
+            <button
+              className={this.togglePlayButton()}
+              onClick={e => togglePlay()}>
+            </button>
+            <button
+              className='next-song'
+              onClick={e => nextSong()}>
+            </button>
+            <button
+              className='mute'
+              onClick={e => toggleMute()}>
+            </button>
           </div>
           <div id='player-seek'>1:00 ---------+-------------- 3:00</div>
         </div>
