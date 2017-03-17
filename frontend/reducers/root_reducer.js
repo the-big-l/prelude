e96a349f7381b437e0bb0f8ce26f8974d2f9a815
Leo Salat <leo.salat@gmail.com>
2017-03-16 17:21:19 -0700

    add player reducer

--- frontend/reducers/root_reducer.js
+++ frontend/reducers/root_reducer.js
@@ -1,8 +1,10 @@
 import {combineReducers} from 'redux';
 import sessionReducer from './session_reducer';
+import playerReducer from './player_reducer';
 
 const rootReducer = combineReducers({
-  session: sessionReducer
+  session: sessionReducer,
+  player: playerReducer
 });
 
 export default rootReducer;
