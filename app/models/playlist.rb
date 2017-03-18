6521e85191b789ffb144ae83aaeb7d1633b8f074
Leo Salat <leo.salat@gmail.com>
2017-03-18 12:23:01 -0700

    add playlists

---
+++ app/models/playlist.rb
@@ -0,0 +1,17 @@
+# == Schema Information
+#
+# Table name: playlists
+#
+#  id         :integer          not null, primary key
+#  name       :string           not null
+#  user_id    :integer          not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
+class Playlist < ApplicationRecord
+  validates :name, :user_id, presence: true
+
+  belongs_to :user
+  has
+end
