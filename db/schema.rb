9bd67ee89478a1ff276c7a9a6676977a600ef71d
Leo Salat <leo.salat@gmail.com>
2017-03-18 13:36:45 -0700

    update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170318190302) do
+ActiveRecord::Schema.define(version: 20170318203254) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,15 @@ ActiveRecord::Schema.define(version: 20170318190302) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "playlist_members", force: :cascade do |t|
+    t.integer  "playlist_id", null: false
+    t.integer  "track_id",    null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.index ["playlist_id"], name: "index_playlist_members_on_playlist_id", using: :btree
+    t.index ["track_id"], name: "index_playlist_members_on_track_id", using: :btree
+  end
+
   create_table "playlists", force: :cascade do |t|
     t.string   "name",       null: false
     t.integer  "user_id",    null: false
@@ -43,6 +52,7 @@ ActiveRecord::Schema.define(version: 20170318190302) do
     t.integer  "album_id",   null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "track_url"
     t.index ["album_id"], name: "index_tracks_on_album_id", using: :btree
     t.index ["artist_id"], name: "index_tracks_on_artist_id", using: :btree
   end
