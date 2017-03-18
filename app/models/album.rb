8614aff811e7efabdec61f36047b9ffc31308473
Leo Salat <leo.salat@gmail.com>
2017-03-18 12:23:15 -0700

    add albums

---
+++ app/models/album.rb
@@ -0,0 +1,17 @@
+# == Schema Information
+#
+# Table name: albums
+#
+#  id         :integer          not null, primary key
+#  title      :string           not null
+#  artist_id  :integer          not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
+class Album < ApplicationRecord
+  validates :title, :artist_id, presence: true
+
+  has_many :tracks
+  belongs_to :artist
+end
