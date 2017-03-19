7fd900dda082fbe2f771e6509d9f15f9a97d5fba
Leo Salat <leo.salat@gmail.com>
2017-03-19 09:05:24 -0700

    add async request

--- frontend/components/app/main/song_list/song_list_container.js
+++ frontend/components/app/main/song_list/song_list_container.js
@@ -1,12 +1,13 @@
 import { connect } from 'react-redux';
 import SongList from './song_list';
+import { requestSongs } from '../../../../actions/song_list_actions';
 
 const mapStateToProps = ({songList}) => ({
   songList
 });
 
 const mapDispatchToProps = dispatch => ({
-
+  requestSongs: () => dispatch(requestSongs())
 });
 
 export default connect(
