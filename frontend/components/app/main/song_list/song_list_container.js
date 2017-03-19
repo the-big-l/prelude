80d940a80ca1bdbd9d67fa67ecbc82a4bb6af6cb
Leo Salat <leo.salat@gmail.com>
2017-03-18 20:38:22 -0700

    add song_list_container.js

---
+++ frontend/components/app/main/song_list/song_list_container.js
@@ -0,0 +1,15 @@
+import { connect } from 'react-redux';
+import SongList from './song_list';
+
+const mapStateToProps = props => ({
+
+});
+
+const mapDispatchToProps = dispatch => ({
+
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(SongList);
