5a66ca068ca064d73d86c34d2d042c78188980d1
Leo Salat <leo.salat@gmail.com>
2017-03-18 20:38:02 -0700

    add song_list.jsx

---
+++ frontend/components/app/main/song_list/song_list.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class SongList extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className='song-list'>
+        <h1>this is all the songs in the thing</h1>
+      </div>
+    );
+  }
+}
+
+export default SongList;
