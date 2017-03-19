216ba60ccbff14c78af38648886eb7b1d6c2b1dd
Leo Salat <leo.salat@gmail.com>
2017-03-18 21:35:19 -0700

    add songListReducer to root

--- frontend/reducers/root_reducer.js
+++ frontend/reducers/root_reducer.js
@@ -1,10 +1,12 @@
 import {combineReducers} from 'redux';
 import sessionReducer from './session_reducer';
 import playerReducer from './player_reducer';
+import songListReducer from './song_list_reducer';
 
 const rootReducer = combineReducers({
   session: sessionReducer,
-  player: playerReducer
+  player: playerReducer,
+  songList: songListReducer
 });
 
 export default rootReducer;
