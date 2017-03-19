080b140c41c539f512e956ab0fa11b12df506a7c
Leo Salat <leo.salat@gmail.com>
2017-03-19 09:04:51 -0700

    add asynch request

--- frontend/actions/song_list_actions.js
+++ frontend/actions/song_list_actions.js
@@ -1,6 +1,14 @@
+import fetchSongs from '../util/song_util';
 export const POPULATE_SONG_LIST = "POPULATE_SONG_LIST";
 
+// sync
 export const populateSongList = songList => ({
   type: POPULATE_SONG_LIST,
   songList
 });
+
+// async
+export const requestSongs = () => dispatch => (
+  fetchSongs()
+    .then(songList => dispatch(populateSongList(songList)))
+);
