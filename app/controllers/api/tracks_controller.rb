ccccde34644bbf6be8746e9bf8609ce7a17292bc
Leo Salat <leo.salat@gmail.com>
2017-03-18 18:43:31 -0700

    add album/artist info and order results

--- app/controllers/api/tracks_controller.rb
+++ app/controllers/api/tracks_controller.rb
@@ -1,5 +1,7 @@
 class Api::TracksController < ApplicationController
   def index
     @tracks = Track.all
+      .includes(:artist, :album)
+      .order('artists.name', 'albums.title', :track_no)
   end
 end
