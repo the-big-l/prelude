1c28ae3e892de29e7040c0f6c9ab3abde6941c98
Leo Salat <leo.salat@gmail.com>
2017-03-19 23:05:56 -0700

    add create method

--- app/controllers/api/playlist_members_controller.rb
+++ app/controllers/api/playlist_members_controller.rb
@@ -1,2 +1,15 @@
 class Api::PlaylistMembersController < ApplicationController
+  def create
+    @playlist_member = PlaylistMember.new(playlist_member_params)
+
+    if @playlist_member.save
+      render json: ['successfully saved'], status: 200
+    else
+      render json: @playlist_member.errors.full_messages, status: 422
+    end
+  end
+
+  def playlist_member_params
+    params.require(:playlist_member).permit(:song_id, :playlist_id)
+  end
 end
