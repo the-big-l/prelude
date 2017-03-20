e219dbc0efcd4fc6a81e58d2a0d53bfdd970e9e1
Leo Salat <leo.salat@gmail.com>
2017-03-19 23:10:09 -0700

    add click and off click handlers

--- frontend/components/app/main/song_list/playlist_dropdown.jsx
+++ frontend/components/app/main/song_list/playlist_dropdown.jsx
@@ -1,6 +1,44 @@
-<ul className='playlist-dropdown'>
-  <li>playlist 1</li>
-  <li>playlist 2</li>
-</ul>
+import React from 'react';
+import enhanceWithClickOutside from 'react-click-outside';
+
+class PlaylistDropdown extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleAdd = this.handleAdd.bind(this);
+  }
+
+  componentWillMount() {
+    this.props.requestUserPlaylists(currentUser);
+  }
+
+  handleAdd(playlistId) {
+    const that = this;
+    return e => {
+      const playlist_member = {
+        song_id: this.props.songId,
+        playlist_id: playlistId
+      };
+      that.props.addToPlaylist({playlist_member});
+    };
+  }
+
+  handleClickOutside() {
+    this.props.toggleDropdown();
+  }
+
+  render() {
+    const playlists = this.props.userPlaylists.map((playlist, idx) => {
+      return (
+        <li onClick={this.handleAdd(playlist.id)} key={idx}>{playlist.name}</li>
+      );
+    });
+
+    return (
+      <ul className='playlist-dropdown'>
+        {playlists}
+      </ul>
+    );
+  }
+}
 
 export default enhanceWithClickOutside(PlaylistDropdown);
