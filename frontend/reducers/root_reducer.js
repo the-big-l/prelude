a66ea26092819b26733a4f8276dfd2d1c5b4d58b
Leo Salat <leo.salat@gmail.com>
2017-03-19 23:15:03 -0700

    add playlist reducer

--- frontend/reducers/root_reducer.js
+++ frontend/reducers/root_reducer.js
@@ -2,11 +2,13 @@ import {combineReducers} from 'redux';
 import sessionReducer from './session_reducer';
 import playerReducer from './player_reducer';
 import songListReducer from './song_list_reducer';
+import playlistReducer from './playlist_reducer';
 
 const rootReducer = combineReducers({
   session: sessionReducer,
   player: playerReducer,
-  songList: songListReducer
+  songList: songListReducer,
+  userPlaylists: playlistReducer
 });
 
 export default rootReducer;
