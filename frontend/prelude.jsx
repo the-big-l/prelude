46947a1cd95dc395c31a35f27e787721bb9cb80e
Leo Salat <leo.salat@gmail.com>
2017-03-20 11:01:20 -0700

    add element for modal

--- frontend/prelude.jsx
+++ frontend/prelude.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './store/store';
 import Root from './components/root';
+import Modal from 'react-modal';
 
 // test
 import { requestUserPlaylists, addToPlaylist } from './actions/playlist_actions';
@@ -9,6 +10,7 @@ import { fetchUserPlaylists } from './util/playlist_util';
 
 document.addEventListener('DOMContentLoaded', () => {
     const root = document.getElementById('root');
+    Modal.setAppElement(document.body);
 
     const store = window.currentUser ?
       configureStore({session: {currentUser: window.currentUser}}) :
