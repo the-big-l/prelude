6f1e17633c27c9c644e5abf702d4a6f907a1daae
Leo Salat <leo.salat@gmail.com>
2017-03-20 17:35:26 -0700

    add description to playlists

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170319020238) do
+ActiveRecord::Schema.define(version: 20170321003353) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,10 +39,11 @@ ActiveRecord::Schema.define(version: 20170319020238) do
   end
 
   create_table "playlists", force: :cascade do |t|
-    t.string   "name",       null: false
-    t.integer  "user_id",    null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string   "name",        null: false
+    t.integer  "user_id",     null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.text     "description"
     t.index ["user_id"], name: "index_playlists_on_user_id", using: :btree
   end
 
