c118329cea99d021258f9b6fdba7ac7133c88f18
Leo Salat <leo.salat@gmail.com>
2017-03-20 17:49:42 -0700

    add description to playlists

--- app/controllers/api/playlists_controller.rb
+++ app/controllers/api/playlists_controller.rb
@@ -4,12 +4,23 @@ class Api::PlaylistsController < ApplicationController
   end
 
   def create
+    @playlist = Playlist.new(playlist_params)
+    @playlist.user = current_user
 
+    if @playlist.save
+      render json: ['successfully saved'], status: 200
+    else
+      render json: @playlist.errors.full_messages, status: 422
+    end
   end
 
   def show
   end
 
   def update
+  end
+
+  def playlist_params
+    params.require(:playlist).permit(:name, :description)
   end
 end
