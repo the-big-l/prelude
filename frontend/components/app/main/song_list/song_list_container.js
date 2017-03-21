40397930f94ce8a93040732a0c4456da296350f5
Leo Salat <leo.salat@gmail.com>
2017-03-21 13:15:25 -0700

    add ability to add songs to current player queue

--- frontend/components/app/main/song_list/song_list_container.js
+++ frontend/components/app/main/song_list/song_list_container.js
@@ -1,16 +1,19 @@
 import { connect } from 'react-redux';
 import SongList from './song_list';
 import { requestSongs } from '../../../../actions/song_list_actions';
+import {replaceQueue} from '../../../../actions/player_actions';
 
 const mapStateToProps = ({songList}) => ({
   songList,
   description: 'All the songs',
   title: 'SONGS',
-  type: 'browse'
+  type: 'browse',
+  author: 'Prelude'
 });
 
 const mapDispatchToProps = dispatch => ({
-  requestSongs: () => dispatch(requestSongs())
+  requestSongs: () => dispatch(requestSongs()),
+  replaceQueue: (newQueue) => dispatch(replaceQueue(newQueue))
 });
 
 export default connect(
