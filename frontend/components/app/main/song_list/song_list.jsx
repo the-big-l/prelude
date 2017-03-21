36a3dc30583504ddf8b98c88e58461f3bb53dc91
Leo Salat <leo.salat@gmail.com>
2017-03-21 11:37:39 -0700

    refactor to take in additional props as info

--- frontend/components/app/main/song_list/song_list.jsx
+++ frontend/components/app/main/song_list/song_list.jsx
@@ -8,27 +8,43 @@ class SongList extends React.Component {
   }
 
   componentWillMount() {
-    this.props.requestSongs();
+    this.props.requestSongs(this.params());
   }
 
+  componentWillReceiveProps(nextProps) {
+    if (this.props.params.id !== nextProps.params.id) {
+      this.props.requestSongs(nextProps.params.id);
+    }
+    console.log('song list');
+    console.log(this.props.songList);
+  }
+
+  params() {
+    if (this.props.params.id) return this.props.params.id;
+  }
+
+  isPlaylist() {
+    return this.props.type === 'playlist';
+  }
 
   render() {
     const isPlaylist = false;
+    const {description, songList, title, type } = this.props;
 
     return (
       <div className='song-list'>
         <header className='song-list'>
           <ListBillboard
-            type={'browse'}
-            title={'SONGS'}
-            subTitle={'All the songs'}
+            type={type}
+            title={title}
+            subTitle={description}
             author={'Prelude'}
-            count={this.props.songList.length}
+            count={songList.length}
             />
         </header>
         <div className='play-follow'>
           <button className='play'>Play</button>
-          {isPlaylist ? <button>Follow</button> : null}
+          {this.isPlaylist() ? <button>Follow</button> : null}
         </div>
         <SongListIndex songList={this.props.songList} />
       </div>
