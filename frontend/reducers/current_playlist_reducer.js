c5f5d1a7c2070f93a953105b8aa1cf4e24bb7fa3
Leo Salat <leo.salat@gmail.com>
2017-03-20 21:11:50 -0700

    add current_playlist_reducer.js

---
+++ frontend/reducers/current_playlist_reducer.js
@@ -0,0 +1,25 @@
+import { RECEIVE_PLAYLIST, RECEIVE_PLAYLISTS_ERRORS } from '../actions/playlist_actions';
+import merge from 'lodash/merge';
+
+const _nullPlaylist = {
+  errors: [],
+  name: null,
+  description: null,
+  songs: []
+};
+
+const currentPlaylistReducer = (state = _nullPlaylist, action) => {
+  Object.freeze(state)
+  const newState = merge({}, state);
+  switch(action.type) {
+    case RECEIVE_PLAYLISTS_ERRORS:
+      const errors = action.errors;
+      return merge({}, _nullPlaylist, {errors});
+    case RECEIVE_PLAYLIST:
+      return merge({}, state, action.playlist)
+    default:
+      return state;
+  }
+};
+
+export default currentPlaylistReducer;
