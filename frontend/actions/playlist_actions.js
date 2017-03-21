4e1e6c9db88ab8b41925977d4223984fc054d894
Leo Salat <leo.salat@gmail.com>
2017-03-20 21:12:15 -0700

    add new playlist and get playlist actions

--- frontend/actions/playlist_actions.js
+++ frontend/actions/playlist_actions.js
@@ -1,21 +1,42 @@
-import { sendPlaylistMember, fetchUserPlaylists } from '../util/playlist_util';
+import * as PlayerUtil from '../util/playlist_util';
 
 export const RECEIVE_PLAYLISTS = 'RECEIVE_PLAYLISTS';
+export const RECEIVE_PLAYLISTS_ERRORS = 'RECEIVE_PLAYLISTS_ERRORS';
+export const RECEIVE_PLAYLIST = 'RECEIVE_PLAYLIST';
 
 export const receivePlaylists = userPlaylists => ({
   type:RECEIVE_PLAYLISTS,
   userPlaylists
 });
 
+export const receivePlaylist = playlist => ({
+  type:RECEIVE_PLAYLIST,
+  playlist
+});
+
+export const receivePlaylistsErrors = errors => ({
+  type:RECEIVE_PLAYLISTS_ERRORS,
+  errors
+});
+
 // async
-export const requestUserPlaylists = (user) => dispatch => (
-  fetchUserPlaylists(user)
+export const createPlaylist = playlist => dipatch (
+  PlayerUtil.sendPlaylist(playlist)
+    .then(playlist => dispatch(receivePlaylist(playlist)))
+    .fail(errors => dispatch(receivePlaylistErrors(errors)))
+);
+
+export const requestUserPlaylists = user => dispatch => (
+  PlayerUtil.fetchUserPlaylists(user)
     .then(userPlaylists => dispatch(receivePlaylists(userPlaylists)))
 );
 
-export const addToPlaylist = (playlistMember) => dispatch => {
-  sendPlaylistMember(playlistMember);
+export const requestPlaylist = playlist => dispatch => (
+  PlayerUtil.fetchPlaylist(playlist)
+    .then(playlist => dispatch(receivePlaylist(playlist)))
+    .then(playlist => dispatch(receiveSongList(playlist.songs)))
+);
+
+export const addToPlaylist = playlistMember => dispatch => {
+  PlayerUtil.sendPlaylistMember(playlistMember);
 };
-
-
-export const doNothing = () => {};
