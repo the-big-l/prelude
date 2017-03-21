cf1ce68dd03125b774f87356012fdf25a3f2028c
Leo Salat <leo.salat@gmail.com>
2017-03-21 13:17:17 -0700

    add ability to repalce player queue; add author to media info

--- frontend/components/app/main/playlist/playlist_container.js
+++ frontend/components/app/main/playlist/playlist_container.js
@@ -1,16 +1,19 @@
 import { connect } from 'react-redux';
 import SongList from '../song_list/song_list';
 import { requestPlaylist } from '../../../../actions/playlist_actions';
+import {replaceQueue} from '../../../../actions/player_actions';
 
 const mapStateToProps = ({currentPlaylist}, ownProps) => ({
   songList: currentPlaylist.songs,
   description: currentPlaylist.description,
   title: currentPlaylist.name,
-  type: 'playlist'
+  type: 'playlist',
+  author: currentPlaylist.author
 });
 
 const mapDispatchToProps = dispatch => ({
-  requestSongs: (playlistId) => dispatch(requestPlaylist(playlistId))
+  requestSongs: playlistId => dispatch(requestPlaylist(playlistId)),
+  replaceQueue: newQueue => dispatch(replaceQueue(newQueue))
 });
 
 export default connect(
