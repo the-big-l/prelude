e01747cbd3e0ad5638c993b42beb8dc9b5230ddd
Leo Salat <leo.salat@gmail.com>
2017-03-21 23:49:46 -0700

    add null song to initial state

--- frontend/reducers/player_reducer.js
+++ frontend/reducers/player_reducer.js
@@ -37,7 +37,7 @@ const _defaultPlayer = Object.freeze({
   mute: false,
   volume: 1.0,
   currentSong: 0,
-  queue: sampleQueue
+  queue: [{song: {title: '', album: ''}}]
 });
 
 const _shuffleSongs = q => {
