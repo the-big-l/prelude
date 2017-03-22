12e27963251a32dbe5ddc96b8ce1f8c164c28876
Leo Salat <leo.salat@gmail.com>
2017-03-22 11:05:28 -0700

    add delete playlist call

--- frontend/util/playlist_util.js
+++ frontend/util/playlist_util.js
@@ -20,6 +20,13 @@ export const sendPlaylist = (playlist) => {
   });
 }
 
+export const deletePlaylist = (playlistId) => {
+  return $.ajax({
+    method: 'DELETE',
+    url: `/api/playlists/${playlistId}`
+  });
+}
+
 export const sendPlaylistMember = (playlist_member) => {
   return $.ajax({
     method: 'POST',
