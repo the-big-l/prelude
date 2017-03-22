0a8b6fd4120297497eb022b0d25ec330f71b8578
Leo Salat <leo.salat@gmail.com>
2017-03-22 11:05:04 -0700

    add add_to_list action

--- frontend/reducers/song_list_reducer.js
+++ frontend/reducers/song_list_reducer.js
@@ -1,13 +1,17 @@
 import { POPULATE_SONG_LIST } from '../actions/song_list_actions';
-import { REMOVE_SONG } from '../actions/playlist_actions';
+import { REMOVE_SONG, ADD_TO_LIST } from '../actions/playlist_actions';
 
 const _emptySongList = [];
 
 const songListReducer = (state = _emptySongList, action) => {
   Object.freeze(state)
+  const newState = state.slice();
   switch(action.type) {
     case POPULATE_SONG_LIST:
       return action.listItems;
+    case ADD_TO_LIST:
+      newState.push(action.listItem);
+      return newState;
     case REMOVE_SONG:
       return state.filter(listItem => (listItem.member_id !== action.member.id));
     default:
