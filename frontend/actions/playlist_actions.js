e7ecbde1a0c26fb677dc34944f43a9f569da6cfe
Leo Salat <leo.salat@gmail.com>
2017-03-21 22:29:28 -0700

    add remove song functions; update util alias

--- frontend/actions/playlist_actions.js
+++ frontend/actions/playlist_actions.js
@@ -1,9 +1,10 @@
-import * as PlayerUtil from '../util/playlist_util';
+import * as Playlist from '../util/playlist_util';
 import { populateSongList } from './song_list_actions.js';
 
 export const RECEIVE_PLAYLISTS = 'RECEIVE_PLAYLISTS';
 export const RECEIVE_PLAYLIST_ERRORS = 'RECEIVE_PLAYLIST_ERRORS';
 export const RECEIVE_PLAYLIST = 'RECEIVE_PLAYLIST';
+export const REMOVE_SONG = 'REMOVE_SONG';
 
 export const receivePlaylists = userPlaylists => ({
   type:RECEIVE_PLAYLISTS,
@@ -20,27 +21,36 @@ export const receivePlaylistErrors = errors => ({
   errors
 });
 
+export const deletePlaylistMember = member => ({
+  type:REMOVE_SONG,
+  member
+})
+
 // async
 export const createPlaylist = playlist => dispatch => (
-  PlayerUtil.sendPlaylist(playlist)
+  Playlist.sendPlaylist(playlist)
     .then(playlist => dispatch(receivePlaylist(playlist)))
     .fail(errors => dispatch(receivePlaylistErrors(errors)))
 );
 
 export const requestUserPlaylists = user => dispatch => (
-  PlayerUtil.fetchUserPlaylists(user)
+  Playlist.fetchUserPlaylists(user)
     .then(userPlaylists => dispatch(receivePlaylists(userPlaylists)))
 );
 
 export const requestPlaylist = playlistId => dispatch => (
-  PlayerUtil.fetchPlaylist(playlistId)
+  Playlist.fetchPlaylist(playlistId)
     .then(playlist => {
       dispatch(receivePlaylist(playlist));
-      dispatch(populateSongList(playlist.songs));
+      dispatch(populateSongList(playlist.members));
     })
-    // .then(playlist => dispatch(populateSongList(playlist.songs)))
+);
+
+export const removeSong = memberId => dispatch => (
+  Playlist.deletePlaylistMember(memberId)
+    .then(member => dispatch(deletePlaylistMember(member)))
 );
 
 export const addToPlaylist = playlistMember => dispatch => {
-  PlayerUtil.sendPlaylistMember(playlistMember);
+  Playlist.sendPlaylistMember(playlistMember);
 };
