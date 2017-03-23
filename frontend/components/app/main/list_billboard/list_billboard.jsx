716d098da6f7dceeb749fc8ae9cae8e9baebb11b
Leo Salat <leo.salat@gmail.com>
2017-03-22 19:36:46 -0700

    move out of song list dir

---
+++ frontend/components/app/main/list_billboard/list_billboard.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+class ListBillboard extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    const {
+      type,
+      title,
+      subTitle,
+      footer } = this.props;
+
+    return (
+      <div className='list-billboard'>
+        <figure className='album-art'>picture here</figure>
+        <aside className='media-info'>
+          <h4 className='type'>{type}</h4>
+          <h1 className='title'>{title}</h1>
+          <p className='sub-title'>{subTitle}</p>
+          <p className='meta'>{footer}</p>
+        </aside>
+      </div>
+    );
+  }
+}
+
+export default ListBillboard;
