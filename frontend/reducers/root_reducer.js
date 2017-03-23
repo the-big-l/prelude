caa29eb68aa3f915626473c456eca53a8b4a3766
Leo Salat <leo.salat@gmail.com>
2017-03-23 09:18:36 -0700

    add followed playlists

--- frontend/reducers/root_reducer.js
+++ frontend/reducers/root_reducer.js
@@ -2,17 +2,19 @@ import {combineReducers} from 'redux';
 import sessionReducer from './session_reducer';
 import playerReducer from './player_reducer';
 import songListReducer from './song_list_reducer';
-import playlistReducer from './playlist_reducer';
+import userPlaylistReducer from './user_playlist_reducer';
 import currentPlaylistReducer from './current_playlist_reducer';
 import usersReducer from './users_reducer';
+import usersReducer from './followed_playlist_reducer';
 
 const rootReducer = combineReducers({
   session: sessionReducer,
   player: playerReducer,
   listItems: songListReducer,
-  userPlaylists: playlistReducer,
+  userPlaylists: userPlaylistsReducer,
   currentPlaylist: currentPlaylistReducer,
-  users: usersReducer
+  users: usersReducer,
+  followedPlaylists: followedPlaylistReducer
 });
 
 export default rootReducer;
