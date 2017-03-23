c25895e51834a24e4828bc44b56f6b7df67b7d64
Leo Salat <leo.salat@gmail.com>
2017-03-23 00:21:41 -0700

    add assoc

--- app/models/playlist_follow.rb
+++ app/models/playlist_follow.rb
@@ -8,6 +8,9 @@
 #  created_at  :datetime         not null
 #  updated_at  :datetime         not null
 #
-
 class PlaylistFollow < ApplicationRecord
+  validates :playlist_id, :user_id, presence: true
+
+  belongs_to :user
+  belongs_to :playlist
 end
