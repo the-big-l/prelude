d75f6255be12d576071bb043581a1baf304b0fa5
Leo Salat <leo.salat@gmail.com>
2017-03-23 00:42:57 -0700

    add index route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
-    resources :playlist_follows, only: [:create, :destroy]
+    resources :playlist_follows, only: [:create, :destroy, :index]
     resources :playlist_members, only: [:create, :destroy]
     resources :playlist_follows, only: [:create, :destroy]
     resources :playlists, only: [:create, :index, :show, :update, :destroy]
