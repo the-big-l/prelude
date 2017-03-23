735726039e92976f99cbba1e4f3bbbddd7974f43
Leo Salat <leo.salat@gmail.com>
2017-03-22 20:51:52 -0700

    add playlist count

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -15,7 +15,7 @@ class Api::UsersController < ApplicationController
   end
 
   def show
-    @user = User.find(params[:id])
+    @user = User.includes(:playlists).find(params[:id])
   end
 
   private
