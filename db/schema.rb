02b755c06b35d5f14d47502c8a5c6c1ed0abc34e
Leo Salat <leo.salat@gmail.com>
2017-03-23 11:00:29 -0700

    add unique index [user, playlist]

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170323070004) do
+ActiveRecord::Schema.define(version: 20170323175643) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 20170323070004) do
     t.integer  "user_id",     null: false
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.index ["playlist_id", "user_id"], name: "index_playlist_follows_on_playlist_id_and_user_id", unique: true, using: :btree
     t.index ["playlist_id"], name: "index_playlist_follows_on_playlist_id", using: :btree
     t.index ["user_id"], name: "index_playlist_follows_on_user_id", using: :btree
   end
