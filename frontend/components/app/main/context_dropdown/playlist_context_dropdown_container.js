06bcd82fcb60c8705cc6e2deb61150926cbdf7c4
Leo Salat <leo.salat@gmail.com>
2017-03-23 15:54:06 -0700

    update playlist actions name

--- frontend/components/app/main/context_dropdown/playlist_context_dropdown_container.js
+++ frontend/components/app/main/context_dropdown/playlist_context_dropdown_container.js
@@ -1,6 +1,6 @@
 import { connect } from 'react-redux';
 import ContextDropdown from './context_dropdown';
-import { removePlaylist } from '../../../../actions/playlist_actions';
+import { removePlaylist } from '../../../../actions/current_playlist_actions';
 import { hashHistory } from 'react-router';
 
 const mapStateToProps = ({currentPlaylist}, ownProps) => ({
