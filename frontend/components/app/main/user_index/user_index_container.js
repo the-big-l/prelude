1cf5dc0f36b476d377d6b6d3c66f8bfb412dc339
Leo Salat <leo.salat@gmail.com>
2017-03-22 20:56:06 -0700

    add requestCollection action

--- frontend/components/app/main/user_index/user_index_container.js
+++ frontend/components/app/main/user_index/user_index_container.js
@@ -1,12 +1,12 @@
 import { connect } from 'react-redux';
 import UserIndex from './user_index';
+import {requestUsers} from '../../../../actions/user_actions';
 
 const mapStateToProps = props => ({
-
 });
 
 const mapDispatchToProps = dispatch => ({
-
+  requestCollection: () => dispatch(requestUsers())
 });
 
 export default connect(
