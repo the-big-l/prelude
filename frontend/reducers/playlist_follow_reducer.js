fe5ec2c6fb30335338e9072cf3665f8dbed15f10
Leo Salat <leo.salat@gmail.com>
2017-03-23 15:48:17 -0700

    add receive and remove playlist action

--- frontend/reducers/playlist_follow_reducer.js
+++ frontend/reducers/playlist_follow_reducer.js
@@ -1,12 +1,22 @@
-import { RECEIVE_FOLLOWED_PLAYLISTS } from '../actions/playlist_follow_actions';
+import {
+  RECEIVE_FOLLOWED_PLAYLISTS,
+  RECEIVE_FOLLOWED_PLAYLIST,
+  REMOVE_FOLLOWED_PLAYLIST} from '../actions/playlist_follow_actions';
+import merge from 'lodash/merge';
 
 const _emptyList = [];
 
 const playlistFollowReducer = (state = _emptyList, action) => {
   Object.freeze(state)
+  const newState = state.slice();
   switch(action.type) {
     case RECEIVE_FOLLOWED_PLAYLISTS:
       return action.followedPlaylists;
+    case RECEIVE_FOLLOWED_PLAYLIST:
+      newState.push(action.followedPlaylist);
+      return newState;
+    case REMOVE_FOLLOWED_PLAYLIST:
+      return newState.filter(pl => (pl.id !== action.followedPlaylist.id));
     default:
       return state;
   }
