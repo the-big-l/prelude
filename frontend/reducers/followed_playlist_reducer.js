0397cc26d4448159ac466f6d5a7d172c3938ef99
Leo Salat <leo.salat@gmail.com>
2017-03-23 09:20:54 -0700

    add followed_playlist_reducer.js

---
+++ frontend/reducers/followed_playlist_reducer.js
@@ -0,0 +1,15 @@
+import { RECEIVE_FOLLOWED_PLAYLISTS } from '../actions/followed_playlist_actions';
+
+const _emptyList = [];
+
+const userPlaylistReducer = (state = _emptyList, action) => {
+  Object.freeze(state)
+  switch(action.type) {
+    case RECEIVE_FOLLOWED_PLAYLISTS:
+      return action.followedPlaylists;
+    default:
+      return state;
+  }
+};
+
+export default userPlaylistReducer;
