c5daacbfbf606f2a5d4f221daec07152ea542b4d
Leo Salat <leo.salat@gmail.com>
2017-03-23 20:28:41 -0700

    add dropdown modal

--- frontend/components/app/header/app_header.jsx
+++ frontend/components/app/header/app_header.jsx
@@ -1,29 +1,38 @@
 import React from 'react';
 import { Link, withRouter } from 'react-router';
+import ProfileDropdown from './profile_dropdown';
 
 class AppHeader extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      isOpen: false,
+    };
+    this.openModal = this.openModal.bind(this);
+    this.closeModal = this.closeModal.bind(this);
   }
 
-  componentWillReceiveProps(nextProps) {
-    if (!nextProps.loggedIn) {
-      this.props.router.push("/welcome");
-    }
+  openModal() {
+    this.setState({ isOpen: true });
+  }
+
+  closeModal() {
+    this.setState({ isOpen: false });
   }
 
   render() {
+    debugger
     return (
       <header className='app-header'>
         <div className='right-nav'>
-          <div className='current-user-info'>
-            {this.props.currentUser.username}
+          <div onClick={this.openModal} className='current-user-info'>
+            <p>{this.props.currentUser.username}</p><button id='pd-btn'>&or;</button>
           </div>
-          <ul className='user-dropdown'>
-            <li onClick={this.props.logout}>
-              logout
-            </li>
-          </ul>
+          <ProfileDropdown
+            isOpen={this.state.isOpen}
+            closeModal={this.closeModal}
+            logout={this.props.logout}
+            />
         </div>
       </header>
     );
