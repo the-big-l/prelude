f798427961ef8d5c238321d9864c3da4db49bb86
Leo Salat <leo.salat@gmail.com>
2017-03-23 20:35:53 -0700

    update api for current user profile

--- frontend/components/app/main/user_profile/user_profile_container.js
+++ frontend/components/app/main/user_profile/user_profile_container.js
@@ -2,15 +2,16 @@ import { connect } from 'react-redux';
 import UserProfile from './user_profile';
 import {requestUser} from '../../../../actions/user_actions';
 
-const mapStateToProps = ({users: {currentUser}}) => {
-  return({
+const mapStateToProps = ({users: {currentUser}}) => ({
   lbInfo: {
     type: 'User',
+    footer: `${currentUser.playlist_count} playlists`,
     title: `${currentUser.first_name} ${currentUser.last_name}`,
-    subTitle: `Joined: ${currentUser.created_at.substring(0, 7)}`,
-    footer: `${currentUser.playlist_count} playlists`
-  }
-})};
+    subTitle: `Joined: ${currentUser.created_at.substring(0, 7)}`
+  },
+  type: 'user',
+  currentUserId: currentUser.id
+});
 
 const mapDispatchToProps = dispatch => ({
   requestCollection: userId => dispatch(requestUser(userId))
