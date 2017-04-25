b4d6ff6425d8366d978862fe0497390359266955
Leo Salat <leo.salat@gmail.com>
2017-04-24 21:02:52 -0700

    add logo to header

--- frontend/components/app/header/app_header.jsx
+++ frontend/components/app/header/app_header.jsx
@@ -23,6 +23,9 @@ class AppHeader extends React.Component {
   render() {
     return (
       <header className='app-header'>
+        <div className='left-nav'>
+          <img src="https://s3-us-west-1.amazonaws.com/prelude-prd/images/prelude-logo-large.png" alt=""></img>
+        </div>
         <div className='right-nav'>
           <div onClick={this.openModal} className='current-user-info'>
             <p>{this.props.currentUser && this.props.currentUser.username}</p><button id='pd-btn'>&or;</button>
